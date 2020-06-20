aec63cdf2e874623d38361138676b44979a80a4a
Itay <itay@itaysoft.com>
2020-06-20 02:42:04 -0300

    Create Xcode Project

---
+++ Trace2JSON/Trace2JSON/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Trace2JSON
+//
+//  Created by Itay Brenner on 6/20/20.
+//  Copyright © 2020 Itaysoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
